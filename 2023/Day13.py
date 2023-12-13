d22c84a399afc09c29972980f989d1bef3e9ae54
Robin Szemeti <46870724+rszemeti@users.noreply.github.com>
2023-12-13 16:21:30 +0000

    Day13 cleanup

--- 2023/Day13.py
+++ 2023/Day13.py
@@ -1,34 +1,23 @@
-import itertools
-
-
-
 def print_map(m):
     for x in range(0,len(m)):
         print("".join(m[x]))
     print("")
 
 def proc(m,diffs):
-    #print_map(m)
     v =find_v(m,diffs)
     if v:
-        print("Vert: %s" % v)
         return v
     else:
         h = find_h(m,diffs)
         if h:
-            print("Horiz: %s" % h)
             return 100*h
         
-
 def find_v(m,diffs):
     for i in range(0,len(m[0])-1):
         d=0
-        #print("Column %s" % i)
         cells = min(i+1,len(m[0])-i-1)
-        #print(i,cells)
         for r in map:
             for offs in range(1,cells+1):
-                #print(i-offs+1,i+offs)
                 if(r[i-offs+1]!=r[i+offs]):
                     d += 1
         if d==diffs:
@@ -37,20 +26,14 @@ def find_v(m,diffs):
 def find_h(m,diffs):
     for i in range(0,len(m)-1):
         d = 0
-        #print("Row %s" % i)
         cells = min(i+1,len(m)-i-1)
-        #print(i,cells)
         for col in range(0,len(m[0])):
             for offs in range(1,cells+1):
-                #print(i-offs+1,i+offs)
                 if(m[i-offs+1][col]!=m[i+offs][col]):
                     d +=1
                 
         if d==diffs:
             return i+1
-
-            
-        
 
 with open("Day13.txt") as file:
     smudges=1
