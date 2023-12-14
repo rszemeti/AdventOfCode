716bc94f0be7cbcd13a76ea3ecbcda595433f86c
Robin Szemeti <46870724+rszemeti@users.noreply.github.com>
2023-12-14 02:22:23 +0000

    Update Day13.py
    Tidied up with a transpose()

--- 2023/Day13.py
+++ 2023/Day13.py
@@ -1,42 +1,27 @@
-def print_map(m):
-    for x in range(0,len(m)):
-        print("".join(m[x]))
-    print("")
+import numpy as np
 
 def proc(m,diffs):
-    v =find_v(m,diffs)
+    v =find_symmetry(m,diffs)
     if v:
         return v
     else:
-        h = find_h(m,diffs)
+        h = find_symmetry(np.transpose(m),diffs)
         if h:
             return 100*h
         
-def find_v(m,diffs):
+def find_symmetry(m,diffs):
     for i in range(0,len(m[0])-1):
         d=0
         cells = min(i+1,len(m[0])-i-1)
-        for r in map:
+        for r in m:
             for offs in range(1,cells+1):
                 if(r[i-offs+1]!=r[i+offs]):
                     d += 1
         if d==diffs:
             return i+1
 
-def find_h(m,diffs):
-    for i in range(0,len(m)-1):
-        d = 0
-        cells = min(i+1,len(m)-i-1)
-        for col in range(0,len(m[0])):
-            for offs in range(1,cells+1):
-                if(m[i-offs+1][col]!=m[i+offs][col]):
-                    d +=1
-                
-        if d==diffs:
-            return i+1
-
 with open("Day13.txt") as file:
-    smudges=1
+    smudges=0 # 0 for pt1, 1 for pt2
     map = []
     total=0
     for line in file:
